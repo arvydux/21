9e86c12501cc6b769dc6e2cfd883a10e1fb66f05
arvydux <46009007+arvydux@users.noreply.github.com>
2025-05-23 11:28:16 +0300

    Update kasa-orders.blade.php

--- resources/views/livewire/kasa-orders.blade.php
+++ resources/views/livewire/kasa-orders.blade.php
@@ -1,6 +1,13 @@
-<div  class="">
-    <div id="ready-orders"  class="grid md:grid-cols-2 auto-rows-min gap-2">
-
+<div wire:poll.5s class="">
+    <div class="grid md:grid-cols-2 auto-rows-min gap-2">
+        @foreach(\App\Models\OrderNumbers::where('is_ready', true)->where('is_taken', false)->orderBy('updated_at', 'desc')->get() as $order)
+            <div wire:click="makeOrderTaken('{{ $order->number }}')"  class="relative text-center shadow-md  hover:drop-shadow-2xl hover:shadow-md  hover:scale-101
+                aspect-video overflow-hidden rounded-xl  bg-emerald-600/80  dark:border-neutral-700">
+                <div class="flex grid content-center flex-col gap-2 h-full text-white/80 rounded-xl w-full">
+                    <div class="font-semibold text-8xl">{{ $order->number }}</div>
+                </div>
+            </div>
+        @endforeach
     </div>
     <div wire:click="toogleTakenOrders()" class="relative my-4 text-center shadow-md  hover:drop-shadow-2xl hover:shadow-md  hover:scale-101
                  rounded-xl  bg-red-600/70  dark:border-neutral-700">
@@ -22,4 +29,3 @@
         </div>
     @endif
 </div>
-
