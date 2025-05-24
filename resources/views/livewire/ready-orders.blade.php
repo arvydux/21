2ff077fbd655340aa6c9e1761493aab402f9689c
arvydux <arvydas.kavaliauskas83@gmail.com>
2025-05-24 15:02:55 +0300

    Update ready-orders.blade.php

--- resources/views/livewire/ready-orders.blade.php
+++ resources/views/livewire/ready-orders.blade.php
@@ -1,20 +1,21 @@
 <div class="fixed bottom-0 left-0 w-full text-center p-2">
     <div wire:poll.5s  class="grid md:grid-cols-10 auto-rows-min gap-1" >
-            @foreach(\App\Models\OrderNumbers::where('is_ready', true)->where('is_taken', false)->orderBy('updated_at', 'desc')->get() as $order)
-            <div wire:click="makeOrderTaken('{{ $order->number }}')"  class="text-center shadow-md p-3 hover:drop-shadow-2xl hover:shadow-md  hover:scale-101
+        @foreach(\App\Models\OrderNumbers::where('is_ready', true)->where('is_taken', false)->orderBy('updated_at', 'desc')->get() as $order)
+            <div onclick="setTimeout(playAlert, 5000)" wire:click="makeOrderTaken('{{ $order->number }}')"  class="text-center shadow-md p-3 hover:drop-shadow-2xl hover:shadow-md  hover:scale-101
                     aspect-video overflow-hidden rounded-xl  bg-emerald-800/80  dark:border-neutral-700">
                 <div class="flex grid content-center flex-col gap-2 h-full text-white/80 rounded-xl w-full">
                     <div class="font-semibold text-6xl">{{ $order->number }}</div>
                 </div>
             </div>
-                    <script>
-                function playAlert() {
-                    const audio = new Audio('/14.mp3');
-                    audio.play();
-                }
-                playAlert();
-            </script>
         @endforeach
     </div>
 </div>
+<script>
+    function playAlert() {
+        const audio = new Audio('/14.mp3');
+        audio.play();
+    }
+
+</script>
+
 
